a1580d79e0a77791a24d17450050f28a127e12cb
fujiwarayuuki0211 <respectingevery8@gmail.com>
2021-06-16 18:24:45 +0900

    エラーメッセージ出力機能

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '------' },
     { id: 2, name: '家族' },
     { id: 3, name: '仕事' },
     { id: 4, name: '友達' },
