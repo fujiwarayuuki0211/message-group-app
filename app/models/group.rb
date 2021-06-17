8ffb3e7245562c1a239032faa79c6ad5443aa796
fujiwarayuuki0211 <respectingevery8@gmail.com>
2021-06-17 10:13:05 +0900

    バリデーションの設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,7 +7,7 @@ class Group < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :name, presence: true
-  validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
+  validates :name, :image, presence: true
+  validates :user_ids, length: { minimum: 2 , message: 'を1人以上選択してください'}
+  validates :category_id, numericality: { other_than: 1 , message: 'を選択してください'} 
 end
